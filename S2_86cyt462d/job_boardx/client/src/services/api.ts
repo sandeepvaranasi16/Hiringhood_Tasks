9930f37f51a621c9521c1963509a5424079c3a1a
sandeepvaranasi16 <sandeep.varanasi@hiringhood.com>
2025-05-02 22:01:19 +0530

    deleted

--- S2_86cyt462d/job_boardx/client/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true, // important for sending/receiving cookies
-});
-
-export default api;
